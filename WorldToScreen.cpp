81126da2525d44b52fcdc816666dbcee3d4a9337
Zetolac <58708564+Zetolac@users.noreply.github.com>
2021-08-16 21:43:26 +0200

    Create WorldToScreen.cpp
    Updated W2S

---
+++ WorldToScreen.cpp
@@ -0,0 +1,47 @@
+Vector3 ProjectWorldToScreen(Vector3 WorldLocation, Vector3 camrot)
+{
+	Vector3 Screenlocation = Vector3(0, 0, 0);
+	Vector3 Camera;
+
+	auto chain69 = read<uintptr_t>(DriverHandle, processID, Localplayer + 0xa8);
+	uint64_t chain699 = read<uintptr_t>(DriverHandle, processID, chain69 + 8);
+
+	Camera.x = read<float>(DriverHandle, processID, chain699 + 0x7F8);
+	Camera.y = read<float>(DriverHandle, processID, Rootcomp + 0x12C);  
+
+	float test = asin(Camera.x);
+	float degrees = test * (180.0 / M_PI);
+	Camera.x = degrees;
+
+	if (Camera.y < 0)
+		Camera.y = 360 + Camera.y;
+
+	D3DMATRIX tempMatrix = Matrix(Camera);
+	Vector3 vAxisX, vAxisY, vAxisZ;
+
+	vAxisX = Vector3(tempMatrix.m[0][0], tempMatrix.m[0][1], tempMatrix.m[0][2]);
+	vAxisY = Vector3(tempMatrix.m[1][0], tempMatrix.m[1][1], tempMatrix.m[1][2]);
+	vAxisZ = Vector3(tempMatrix.m[2][0], tempMatrix.m[2][1], tempMatrix.m[2][2]);
+
+	uint64_t chain = read<uint64_t>(DriverHandle, processID, Localplayer + 0x70);
+	uint64_t chain1 = read<uint64_t>(DriverHandle, processID, chain + 0x98);
+	uint64_t chain2 = read<uint64_t>(DriverHandle, processID, chain1 + 0x138);
+
+	Vector3 vDelta = WorldLocation - read<Vector3>(DriverHandle, processID, chain2 + 0x10);
+	Vector3 vTransformed = Vector3(vDelta.Dot(vAxisY), vDelta.Dot(vAxisZ), vDelta.Dot(vAxisX));
+
+	if (vTransformed.z < 1.f)
+		vTransformed.z = 1.f;
+
+	float zoom = read<float>(DriverHandle, processID, chain699 + 0x590);
+
+	FovAngle = 80.0f / (zoom / 1.19f);
+	float ScreenCenterX = Width / 2.0f;
+	float ScreenCenterY = Height / 2.0f;
+
+	Screenlocation.x = ScreenCenterX + vTransformed.x * (ScreenCenterX / tanf(FovAngle * (float)M_PI / 360.f)) / vTransformed.z;
+	Screenlocation.y = ScreenCenterY - vTransformed.y * (ScreenCenterX / tanf(FovAngle * (float)M_PI / 360.f)) / vTransformed.z;
+	CameraEXT = Camera;
+
+	return Screenlocation;
+}
